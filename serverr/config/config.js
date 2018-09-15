a68a2fca22c407c05817f73b868964136e717a82
Brayhi <brayhan.bg@gmail.com>
2018-09-15 03:20:13 +0200

    Variable de entorno MONGO

--- serverr/config/config.js
+++ serverr/config/config.js
@@ -20,7 +20,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://brayhi:Brayhidota123@ds259361.mlab.com:59361/cafe'
+    urlDB = process.env.MONGO_URI
 };
 process.env.URLDB = urlDB;
 
